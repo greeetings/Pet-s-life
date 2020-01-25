86e4235ea699ee0f6bb5077c6432caaf1ec152a6
“Vladislav <rdthnbeqjg@mail.ru>
2020-01-25 11:03:23 +0300

    deleted captcha

--- src/main/java/org/example/pet/controller/RegistrationController.java
+++ src/main/java/org/example/pet/controller/RegistrationController.java
@@ -1,28 +1,24 @@
 package org.example.pet.controller;
 
 import org.example.pet.domain.User;
-import org.example.pet.domain.dto.CaptchaResponseDto;
 import org.example.pet.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
-    private final static String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
+
 
     @Autowired
     private UserService userService;
@@ -40,7 +36,6 @@ public class RegistrationController {
     @PostMapping("/registration")
     public String addUser(
             @RequestParam("password2") String passwordConfirm,
-            @RequestParam("g-recaptcha-response") String captchaResponce,
             @Valid User user,
             BindingResult bindingResult,
             Model model
